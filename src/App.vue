106b09919b797f8ef139372f4761daf5e2971595
codnjs444 <81910499+codnjs444@users.noreply.github.com>
2024-08-16 18:07:57 +0900

    init

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <template>
-	<div>gsd</div>
+	<v-container>
+		<v-row style="width: 100%">
+			<v-col cols="12" style="background-color: aqua">
+				{{ item }}
+			</v-col>
+
+			<v-col cols="12">
+				<btn @click="poketmon">몬스터볼</btn>
+			</v-col>
+		</v-row>
+	</v-container>
 </template>
 
-<script>
-export default {
-	setup() {
-		return {};
-	},
+<script setup>
+import { ref } from 'vue';
+
+const item = ref('피카츄');
+const poketmon = () => {
+	item.value += '가라';
 };
 </script>
-
-<style lang="scss" scoped></style>
---
+++ src/lectures/4/CompositionApi.vue
@@ -0,0 +1,35 @@
+<template>
+	<div>
+		<h2>{{ reactiveMessage }}</h2>
+		<v-btn variant="outlined" @click="ReactiveMessage"> 메세지 추가 </v-btn>
+
+		<h2>{{ normalMessage }}</h2>
+		<v-btn variant="outlined" @click="NormalMessage"> 메세지 추가 </v-btn>
+	</div>
+</template>
+
+<script setup>
+import { ref, isRef, onMounted, onBeforeMount } from 'vue';
+
+const reactiveMessage = ref('반응형 메시지');
+let normalMessage = '일반 메시지';
+
+onBeforeMount(() => {
+	console.log('onBeforeMount');
+});
+
+onMounted(() => {
+	console.log('onMounted');
+});
+
+const ReactiveMessage = () => {
+	reactiveMessage.value = reactiveMessage.value + '!';
+	console.log(isRef(reactiveMessage));
+};
+
+const NormalMessage = () => {
+	normalMessage = normalMessage + '!';
+};
+</script>
+
+<style lang="scss" scoped></style>
