724e0751ef9b0cba435cd210f6c0f14d619bd6fb
codnjs444 <81910499+codnjs444@users.noreply.github.com>
2024-08-17 16:17:11 +0900

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,29 @@
 <template>
   <v-row>
-    <v-col cols="12" md="4">
-      <v-card
-        subtitle="This is a card subtitle"
-        text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis est labore voluptatibus!"
-        title="This is a title"
-      ></v-card>
-
-      <div class="text-center text-caption">Using Props Only</div>
+    <v-col cols="12">
+      <span v-html="htmTest"></span>
+      <p v-once> {{ poketMon }} </p>
     </v-col>
-
-    <v-col cols="12" md="4">
-      <v-card>
-        <template v-slot:title>
-          This is a title
-        </template>
-
-        <template v-slot:subtitle>
-          This is a card subtitle
-        </template>
-
-        <template v-slot:text>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis est labore voluptatibus!
-        </template>
-      </v-card>
-
-      <div class="text-center text-caption">Using Slots Only</div>
+    <v-col cols="12">
+      <v-btn @click="throwPoketMon">잡아라</v-btn>
     </v-col>
-
-    <v-col cols="12" md="4">
-      <v-card>
-        <v-card-item>
-          <v-card-title>This is a title</v-card-title>
-
-          <v-card-subtitle>This is a card subtitle</v-card-subtitle>
-        </v-card-item>
-
-        <v-card-text>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis est labore voluptatibus!
-        </v-card-text>
-      </v-card>
-
-      <div class="text-center text-caption">Using Markup Only</div>
+    <v-col cols="12" style="text-align:center;">
+      <v-input>ㅇㅇㅇ</v-input>
     </v-col>
   </v-row>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+
+const poketMon = ref('피카츄')
+
+const throwPoketMon = () => {
+  poketMon.value = poketMon.value + ' 가라'
+}
+
+const htmTest = ref('<strong>강한 문자</strong>')
+</script>
+
+<style lang="scss" scoped>
+</style>
