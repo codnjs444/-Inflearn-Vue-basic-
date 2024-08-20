15c5a879df51abed6f0211d4cadcec6872c88a8a
codnjs444 <81910499+codnjs444@users.noreply.github.com>
2024-08-20 13:17:52 +0900

    study : 반응형 함수

---
+++ src/lectures/7/App_toRef,toRefs.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols = 12 style="background-color:red;">
+        {{ book.author }}
+      </v-col>
+      <v-col cols = 12 style="background-color:red;">
+        {{ author }}
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script setup>
+import {reactive, toRefs} from 'vue'
+
+const book = reactive({
+  author: '데일 카네기',
+  year: '2002',
+  title: '자기관리론',
+  description: "내일의 걱정을 오늘 하지마라",
+  price: '18900',
+})
+
+const { author, year, title, description } = toRefs(book)
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
