48a225a319493b317c3e634ac2919eb639795d51
codnjs444 <81910499+codnjs444@users.noreply.github.com>
2024-08-23 18:01:17 +0900

    add : bind 기능 추가

---
+++ src/lectures/9/App_class_bind.vue
@@ -0,0 +1,32 @@
+<template>
+	<v-container>
+		<v-row :class="{ active: isActive, 'text-danger': hasError}">
+			안녕
+		</v-row>
+		<v-row>
+			<v-btn @click="ChangeActive">변경</v-btn>
+		</v-row>
+		<v-row>
+			<v-btn @click="hasError = !hasError">text-danger</v-btn>
+		</v-row>
+	</v-container>
+</template>
+
+<script setup>
+	import { ref } from 'vue'
+
+	const isActive = ref(true)
+	const ChangeActive = () => {
+		isActive.value = !isActive.value
+	}
+	const hasError = ref(false)
+</script>
+
+<style lang="scss" scoped>
+.active {
+	background-color: chocolate
+}
+.text-danger {
+	color: red
+}
+</style>
---
+++ src/lectures/9/App_style_bind.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-card
+    class="mx-auto"
+    prepend-icon="$vuetify"
+    subtitle="The #1 Vue UI Library"
+    width="400"
+		height="auto"
+  >
+    <template v-slot:title>
+      <span class="font-weight-black">Welcome to Vuetify</span>
+    </template>
+
+    <v-card-text :style="styleObject" class="bg-surface-light pt-4">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis est labore voluptatibus! Eaque cupiditate minima, at placeat totam, magni doloremque veniam neque porro libero rerum unde voluptatem!
+    </v-card-text>
+
+		<v-row class="justify-space-around md-12 mt-2">
+			<v-btn @click="fontSize--" variant="tonal"> minus </v-btn>
+			<v-btn @click="fontSize++" variant="tonal"> plus </v-btn>
+		</v-row>
+		<v-spacer class="mt-5"></v-spacer>
+  </v-card>
+</template>
+
+<script setup>
+	import { computed, ref } from 'vue';
+
+	const fontSize = ref(13)
+
+	const styleObject = computed(() => {
+		return{
+			color: 'red !important',
+			fontSize: fontSize.value + 'px'
+		}
+	})
+</script>
