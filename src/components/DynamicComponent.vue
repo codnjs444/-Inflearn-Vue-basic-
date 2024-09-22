2cc1449bb44e24c6529d7098fc704072e5ba4605
codnjs444 <81910499+codnjs444@users.noreply.github.com>
2024-09-22 12:53:52 +0900

    study : DynamicComponent

---
+++ src/components/DynamicComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-container py-4>
+    <v-btn>사관</v-btn>
+    <v-btn>바나나</v-btn>
+  </v-container>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
